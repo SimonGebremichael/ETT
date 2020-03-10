75eef8c6832ce81253b8ba3f2c23a422d9c31330
Simon Gebremichael <Sgebremichael4919@conestogac.on.ca>
2020-03-10 16:45:38 -0400

    navigation update

--- src/pages/js/nav.js
+++ src/pages/js/nav.js
@@ -2,12 +2,21 @@ window.onload = start;
 function start(){
   var proBG = document.getElementById("proBG").addEventListener("click", showPage);
   var sideAcc = document.getElementById("sideAcc").addEventListener("click", showProfile);
+  
+  var sideAcc = document.getElementById("colour_Scheme").addEventListener("click", function(){
+    document.getElementById("ProfileColours").style.display = "block";
+    document.getElementById("ProfileSideAcc").style.display = "none";
+  });
+
+  var sideAcc = document.getElementById("colourBtn").addEventListener("click", function(){
+    document.getElementById("ProfileSideAcc").style.display = "block";
+    document.getElementById("ProfileColours").style.display = "none";
+  });
 
   function showPage(){
     document.getElementById("upcomming").style.filter = "blur(0)";
     document.getElementById("mainFeed").style.filter = "blur(0)";
     document.getElementById("sideBar").style.filter = "blur(0)";
-    // document.getElementById("header").style.filter = "blur(0)";
     document.getElementById("profile").style.display = "none";
   }
 
@@ -15,42 +24,6 @@ function start(){
     document.getElementById("upcomming").style.filter = "blur(3px)";
     document.getElementById("mainFeed").style.filter = "blur(3px)";
     document.getElementById("sideBar").style.filter = "blur(3px)";
-    // document.getElementById("header").style.filter = "blur(3px)";
     document.getElementById("profile").style.display = "block";
   }
-
 }
-
-
-// // $(document).ready(function(){
-// //   $("#proBG").click(function(){
-// //     $("#upcomming").css("filter","blur(0)");
-// //     $("#mainFeed").css("filter","blur(0)");
-// //     $("#sideBar").css("filter","blur(0)");
-// //     $("#header").css("filter","blur(0)");
-// //     $("#profile").fadeToggle();
-// //     profileDefualt();
-    
-// //   });
-
-// //   $("#sideAcc").click(function(){
-// //     $("#profile").show();
-// //     $("#upcomming").css("filter","blur(3px)");
-// //     $("#mainFeed").css("filter","blur(3px)");
-// //     $("#sideBar").css("filter","blur(3px)");
-// //     $("#header").css("filter","blur(3px)");
-// //   });
-
-
-// //   $("#colour_Scheme").click(function(){
-// //     $("#ProfileColours").show();
-// //     $("#ProfileSideAcc").hide();
-// //   });
-
-// //   $("#colourBtn").click(profileDefualt);
-
-// //   function profileDefualt(){
-// //     $("#ProfileColours").hide();
-// //     $("#ProfileSideAcc").show();
-// //   }
-// });
