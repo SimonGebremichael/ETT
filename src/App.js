075fc26ebe0a04bcaac80644e7bd6a37c62aab16
simon g <simong8080@hotmail.com>
2020-03-30 22:50:36 -0400

    authentication connection

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Teamlead from './pages/Teamlead/Teamlead';
 import Calendar from './pages/calendar/calendarMain';
 import Export from './pages/export/Export';
 import Home from './pages/HomePage/Mainpage';
-import Create from './pages/create/create'
-import er_404 from './pages/404/unfound'
-import Analysis from './pages/analysis/analysis'
-import OffTyper from './pages/offType/offtype_form'
-import pending from './pages/pending/pending'
-import Modify from './pages/modify/modify'
+import Create from './pages/create/create';
+import er_404 from './pages/404/unfound';
+import Analysis from './pages/analysis/analysis';
+import OffTyper from './pages/offType/offtype_form';
+import pending from './pages/pending/pending';
+import Modify from './pages/modify/modify';
+import { GoogleLogin } from 'react-google-login';
 
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
@@ -26,40 +27,32 @@ export default class appy extends React.Component {
   }
 
   componentDidMount = () => {
-  }
 
-  // componentDidUpdate(previousProps, previousState) {
-  // }
 
-  // forceUpdate() {
-  //   alert("forceUpdate");
-  // }
 
-      
-  changeColor () {
-    this.setState({name: "blue"});
+
   }
 
   render() {
 
     return (
       <>
-      <Router>
-        <Header>{this.state}</Header>
-        <Switch>
-          <Route path='/login' exact={true} component={Home} />
-          <Route path='/login/pending' exact={true} component={pending} />
-          <Route path='/dashboard' component={Teamlead} />
-          <Route path='/calendar' component={Calendar} />
-          <Route path='/export' component={Export} />
-          <Route path='/create' component={Create} />
-          <Route path='/analysis' component={Analysis} />
-          <Route path='/offtype' component={OffTyper} />
-          <Route path='/profile/modify' component={Modify} />
-          <Redirect exact from="/" strict to="/login" />
-          <Route path='*' exact={true} component={er_404} />
-        </Switch>
-      </Router>
+        <Router>
+          <Header>{this.state}</Header>
+          <Switch>
+            <Route path='/login' exact={true} component={Home} />
+            <Route path='/login/pending' exact={true} component={pending} />
+            <Route path='/dashboard' component={Teamlead} />
+            <Route path='/calendar' component={Calendar} />
+            <Route path='/export' component={Export} />
+            <Route path='/create' component={Create} />
+            <Route path='/analysis' component={Analysis} />
+            <Route path='/offtype' component={OffTyper} />
+            <Route path='/profile/modify' component={Modify} />
+            <Redirect exact from="/" strict to="/login" />
+            <Route path='*' exact={true} component={er_404} />
+          </Switch>
+        </Router>
       </>
     )
   }
--- src/pages/HomePage/Mainpage.js
+++ src/pages/HomePage/Mainpage.js
@@ -3,6 +3,7 @@ import Tree from "./pics/fullTree.png";
 import goog_img from "./pics/google.png";
 import { Link } from 'react-router-dom';
 import $ from 'jquery';
+import { GoogleLogin } from 'react-google-login';
 
 export default class mainPage extends React.Component {
   constructor(props) {
@@ -26,16 +27,47 @@ export default class mainPage extends React.Component {
 
   }
   render() {
+    const responseGoogle = (response) => {
+      try {
+        console.log(response.googleId);
+        console.log('ID: ' + response.Qt.SU);
+        console.log('Fist Name: ' + response.Qt.vW);
+        console.log('Last Name: ' + response.Qt.wU);
+        console.log('Image URL: ' + response.Qt.UK);
+        console.log('Email: ' + response.Qt.zu);
+        window.location = "http://localhost:3000/login/pending?i=" + response.googleId;
+
+      } catch (e) {
+        console.log(e);
+        console.log(response);
+      }
+    }
+
     return (
       <div>
-        <label style={message}>more connection, more organized, <br />enough complexity</label>
         <div style={signIn}>
-          <Link to="/login/pending">
-            <button style={btn}>
-              <img src={goog_img} style={btnImg} />
-              <label style={btnlbl}>Sign in with google</label>
-            </button>
-          </Link>
+          <label style={message}>more connection, more organized, <br />enough complexity</label>
+          {/* <Link to="/login/pending"> */}
+          <div id="GoogleLogin">
+            <GoogleLogin
+              clientId="1048871087214-t3ttoli7jpjv5ep62qr91ftsh4hf7010.apps.googleusercontent.com"
+              buttonText="Login with google"
+              style={{ marginTop: "50%", color: 'green' }}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
+            />
+          </div>
+          {/* </Link> */}
+
+          <style dangerouslySetInnerHTML={{
+            __html: `
+             #GoogleLogin { 
+              margin-top: 130%;
+              margin-left: 3%;
+              position: absobute;
+             }
+                    `}} />
         </div>
         <div style={treeDisplay}>
           <img src={Tree} style={fullTree} alt="tree" />
@@ -44,116 +76,17 @@ export default class mainPage extends React.Component {
     );
   }
 }
-// var CLIENT_ID = '1048871087214-t3ttoli7jpjv5ep62qr91ftsh4hf7010.apps.googleusercontent.com';
-// var API_KEY = 'AIzaSyA0cfGXh6JoX0lXpYnjgTq09m62vO62TmM';
-// var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-// var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
-
-// var authorizeButton = document.getElementById('authorize_button');
-// var signoutButton = document.getElementById('signout_button');
-// var gapi;
-// function handleClientLoad() {
-//   gapi.load('client:auth2', initClient);
-// }
-// function initClient() {
-//   gapi.client.init({
-//     apiKey: API_KEY,
-//     clientId: CLIENT_ID,
-//     discoveryDocs: DISCOVERY_DOCS,
-//     scope: SCOPES
-//   }).then(function () {
-//     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-//     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-//     authorizeButton.onclick = handleAuthClick;
-//     signoutButton.onclick = handleSignoutClick;
-//   }, function (error) {
-//     appendPre(JSON.stringify(error, null, 2));
-//   });
-// }
-
-// function updateSigninStatus(isSignedIn) {
-//   if (isSignedIn) {
-//     authorizeButton.style.display = 'none';
-//     signoutButton.style.display = 'block';
-//     printUserData();
-//   } else {
-//     authorizeButton.style.display = 'block';
-//     signoutButton.style.display = 'none';
-//   }
-// }
-
-// function handleAuthClick(event) {
-//   gapi.auth2.getAuthInstance().signIn();
-// }
-// function handleSignoutClick(event) {
-//   gapi.auth2.getAuthInstance().signOut();
-// }
-
-// function appendPre(message) {
-//   var pre = document.getElementById('content');
-//   var textContent = document.createTextNode(message + '\n');
-//   pre.appendChild(textContent);
-// }
-
-// function printUserData() {
-
-//   gapi.client.calendar.events.list({
-//     'calendarId': 'primary',
-//     'timeMin': (new Date()).toISOString(),
-//     'showDeleted': false,
-//     'singleEvents': true,
-//     'maxResults': 10,
-//     'orderBy': 'startTime'
-//   }).then(function (response) {
-//     var events = response.result.items;
-//     appendPre('Upcoming events:');
-//     if (events.length > 0) {
-//       for (var i = 0; i < events.length; i++) {
-//         var event = events[i];
-//         var when = event.start.dateTime;
-//         if (!when) {
-//           when = event.start.date;
-//         }
-//         appendPre(event.summary + ' (' + when + ')')
-//       }
-//     } else {
-//       console.log(response);
-//       appendPre('No upcoming events found.');
-//     }
-//   });
-
-//   var profile = gapi.auth2.getAuthInstance().currentUser.get().Qt;
-//   console.log('ID: ' + profile.SU);
-//   console.log('Fist Name: ' + profile.vW);
-//   console.log('Last Name: ' + profile.wU);
-//   console.log('Image URL: ' + profile.UK);
-//   console.log('Email: ' + profile.zu);
-// }
-
-
-const btn = {
-  width: "200px",
-  height: "40px",
-  bottom: "-20%",
-  color: "black",
-  backgroundColor: "white",
-  border: "1px solid black",
-  position: "absolute",
-  bottom: "5%",
-  left: "3%"
-}
 
 const signIn = {
-  width: "20%",
+  width: "30%",
   height: "100%",
   float: "left"
 }
 
 const message = {
-  width: "20%",
   height: "100%",
   float: "left",
-  marginTop: "10%",
+  marginTop: "60%",
   marginLeft: "3%",
   color: "white",
   fontSize: "20px"
@@ -171,18 +104,4 @@ const treeDisplay = {
   width: "50%",
   height: "100%",
   float: "right"
-}
-
-
-const btnImg = {
-  width: "50px",
-  height: "100%",
-  backgroundColor: "black",
-  float: "left",
-  objectFit: "contain"
-}
-
-const btnlbl = {
-  float: "right",
-  margin: "10px 20px 10px 0px"
 }
