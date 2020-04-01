f2a392a89b9a7e40289a9a7b590a1a2462011300
simon g <simong8080@hotmail.com>
2020-03-31 23:32:34 -0400

    getperson api with php grabbed

--- src/pages/HomePage/Mainpage.js
+++ src/pages/HomePage/Mainpage.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Tree from "./pics/fullTree.png";
-import goog_img from "./pics/google.png";
 import { Link } from 'react-router-dom';
 import $ from 'jquery';
 import { GoogleLogin } from 'react-google-login';
+import ApiCalendar from 'react-google-calendar-api';
+import ApiCalendar2 from 'react-google-calendar-api/ApiCalendar';
+var CLIENT_ID = '1048871087214-t3ttoli7jpjv5ep62qr91ftsh4hf7010.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyA0cfGXh6JoX0lXpYnjgTq09m62vO62TmM';
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+
 
 export default class mainPage extends React.Component {
   constructor(props) {
@@ -29,17 +35,52 @@ export default class mainPage extends React.Component {
   render() {
     const responseGoogle = (response) => {
       try {
-        console.log(response.googleId);
-        console.log('ID: ' + response.Qt.SU);
-        console.log('Fist Name: ' + response.Qt.vW);
-        console.log('Last Name: ' + response.Qt.wU);
-        console.log('Image URL: ' + response.Qt.UK);
-        console.log('Email: ' + response.Qt.zu);
-        window.location = "http://localhost:3000/login/pending?i=" + response.googleId;
+        // console.log(response.googleId);
+        // console.log('ID: ' + response.Qt.SU);
+        // console.log('Fist Name: ' + response.Qt.vW);
+        // console.log('Last Name: ' + response.Qt.wU);
+        // console.log('Image URL: ' + response.Qt.UK);
+        // console.log('Email: ' + response.Qt.zu);
 
+
+        // if (ApiCalendar.sign) {
+        //   ApiCalendar.listUpcomingEvents(10)
+        //   ApiCalendar.gapi.load('client:auth2', () => {
+        //     ApiCalendar.gapi.client.init({
+        //       apiKey: API_KEY,
+        //       clientId: CLIENT_ID,
+        //       discoveryDocs: DISCOVERY_DOCS,
+        //       scope: SCOPES
+        //     }).then(function () {
+        //       console.log("success baby");
+        //     }, function (error) {
+        //       console.log(JSON.stringify(error, null, 2));
+        //     });
+        //   });
+
+        //   ApiCalendar.gapi.auth2.getAuthInstance().signIn();
+        //   ApiCalendar.gapi.client.calendar.events.list({
+        //     'calendarId': 'primary',
+        //     'timeMin': (new Date()).toISOString(),
+        //     'showDeleted': false,
+        //     'singleEvents': true,
+        //     'maxResults': 10,
+        //     'orderBy': 'startTime'
+        //   }).then(function (sendBack) {
+        //     var events = sendBack.result.items;
+        //     if (events.length > 0) {
+        //       for (var i = 0; i < events.length; i++) {
+        //         var event = events[i],
+        //           when = event.start.dateTime;
+
+        //         if (!when) { when = event.start.date; }
+        //         console.log(event.summary + ' (' + when + ')');
+        //       }
+        //     } else {  console.log('No upcoming events found.'); }
+        //   });
+        // }
       } catch (e) {
         console.log(e);
-        console.log(response);
       }
     }
 
@@ -52,9 +93,9 @@ export default class mainPage extends React.Component {
             <GoogleLogin
               clientId="1048871087214-t3ttoli7jpjv5ep62qr91ftsh4hf7010.apps.googleusercontent.com"
               buttonText="Login with google"
-              style={{ marginTop: "50%", color: 'green' }}
               onSuccess={responseGoogle}
               onFailure={responseGoogle}
+              isSignedIn={true}
               cookiePolicy={'single_host_origin'}
             />
           </div>
--- src/pages/pending/pending.js
+++ src/pages/pending/pending.js
@@ -1,7 +1,8 @@
 import React, { component } from 'react';
 import css from './style/pend.css'
 import thumpsUp from './pics/thumbs.png'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import loading from './pics/loading.gif'
+import { BrowserRouter as Router, Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 export default class Pending extends React.Component {
@@ -10,48 +11,91 @@ export default class Pending extends React.Component {
     }
 
     componentDidMount() {
-    }
+        document.getElementsByClassName("pending_container")[0].style.display = "none";
+        document.getElementsByClassName("pending_container2")[0].style.display = "block";
+        var url = window.location.href.split("?")[1];
+        var user = url.split("=")[1];
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://localhost:8080/crud/api/getperson.php?i=" + user);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                var data = JSON.parse(xhr.responseText);
+                document.getElementsByClassName("pending_container")[0].style.display = "block";
+                document.getElementsByClassName("pending_container2")[0].style.display = "none";
 
-    componentDidUpdate(prevProps) {
+                if(data.employee.employee_status != "onsite"){
+                    document.getElementById("pending_message").innerHTML = "You will  be notified once approved, Thank you";
+                    document.getElementById("pending_header_message").innerHTML = "Your Account is Currently Pending!";
+                }else{
+                    document.getElementById("pending_message").innerHTML = "your account has been aprroved";
+                    document.getElementById("pending_header_message").innerHTML = "Your all set :)";
+                }
+                document.getElementById("pending_username").innerHTML += data.employee.first_name + ", " + data.employee.last_name;
+                document.getElementById("pending_email").innerHTML += data.employee.email;
+                document.getElementById("pending_id").innerHTML += "ID: " + data.employee.employeeID;
+                document.getElementById("pending_img").src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.Pd0tR4zfjYF6MwkYlbQcyQHaEn%26pid%3DApi&f=1";
+                console.log(data);
+            }
+        }
+        xhr.send();
     }
-
     render() {
         const image = {
             width: "30px",
         }
         const imgProfile = {
-            width: "60px",
-            backgroundColor: "black"
+            width: "100px",
+            height: "100px",
+            backgroundColor: "black",
+            marginLeft: "30%",
+            marginTop: "10%",
+            borderRadius: "50%",
+            border: "3px solid black",
+            backgroundColor: "gray"
+        }
+
+        const bottom_info = {
+            backgroundColor: "black",
+            paddingBottom: "10px",
+            paddingTop: "50px",
+            color: "white"
         }
         return (
+            <>
             <div class="pending_container">
                 <div id="pending_header">
-                    <h2>Your Account is Currently Pending!</h2>
+                    <h2 id="pending_header_message"></h2>
                     <img src={thumpsUp} style={image} />
                 </div><br />
-                <p>You will  be notified once approved, Thank you</p><br /><br />
+                <p id="pending_message"></p><br /><br />
+                <div style={bottom_info}>
+                    <div id="pending_profile" >
+                        <div id="pending_pro_img_container">
+                            <img id="pending_img" src="" style={imgProfile} />
+                        </div>
 
-                <div id="pending_profile">
-                    <div id="pending_pro_img_container">
-                        <img src="" style={imgProfile} />
+                        <div id="pending_pro_detainer">
+                            <p id="pending_username"></p><br />
+                            <p id="pending_email"></p><br />
+                            <p id="pending_id"></p>
+                        </div>
                     </div>
 
-                    <div id="pending_pro_detainer">
-                        <p>Pending user:</p><br />
-                        <p>Email:</p><br />
-                        <p>ID:</p>
+                    <div class="row">
+                        <Link to="/dashboard">
+                            <input type="submit" value="OK" />
+                        </Link>
+                        <Link to="/">
+                            <input type="submit" value="Cancel" />
+                        </Link>
                     </div>
-                </div>
-
-                <div class="row">
-                    <Link to="/dashboard">
-                        <input type="submit" value="OK" />
-                    </Link>
-                    <Link to="/">
-                        <input type="submit" value="Cancel" />
-                    </Link>
                 </div>
             </div>
+            <div class="pending_container2">
+                <img src={loading} id="pending_loading_gif"/>
+            </div>
+            </>
+
         )
     }
 }
